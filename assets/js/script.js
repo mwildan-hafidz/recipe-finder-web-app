a3720fd9a6d6d5c603c9a896c7fd12a9e3ec2edf
mwildan-hafidz <m.wildan.hafidz.bjm@gmail.com>
2025-07-11 17:58:17 +0800

    feat: look for recipes

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+const recipeNameInput = document.querySelector('#recipe-name-input');
+const searchBtn = document.querySelector('#search-btn');
+const recipesContainer = document.querySelector('#recipes-container');
+
+searchBtn.addEventListener('click', async () => {
+    const search = recipeNameInput.value;
+    const recipes = await getRecipes(search);
+    renderRecipes(recipes);
+});
+
+function getRecipes(name) {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+    return fetch(url)
+        .then((res) => {
+            if (!res.ok) throw new Error(res.statusText);
+            return res.json();
+        })
+        .then((json) => {
+            return json.meals;
+        });
+}
+
+function renderRecipes(recipes) {
+    let contents = '';
+    recipes.forEach(recipe => {
+        contents += `<div class="col-6 col-md-4 col-lg-3 col-xl-2">
+            <div class="card h-100">
+                <img src="${recipe.strMealThumb}" alt="mealthumb" class="card-img-top">
+                <div class="card-body border-top">
+                    <p class="card-text mb-0">${recipe.strMeal}</p>
+                    <span class="badge text-bg-warning">${recipe.strCategory}</span>
+                </div>
+            </div>
+        </div>`
+    });
+    recipesContainer.innerHTML = contents;
+}
